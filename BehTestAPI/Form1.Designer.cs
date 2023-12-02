f2d659feae870e87009146fca3f6a4716f4067d1
Moslem Rashidi <rashidi_moslem@yahoo.com>
2023-12-02 12:19:35 +0330

    Add project files.

---
+++ BehTestAPI/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CreateInvDocByAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxGoodCode = new System.Windows.Forms.TextBox();
+            this.tbxGoodQty = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnSubmit);
+            this.flowLayoutPanel1.Controls.Add(this.btnReset);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 65);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(280, 33);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(202, 3);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 25);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "ارسال";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnReset.Location = new System.Drawing.Point(121, 3);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 25);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "شروع مجدد";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "کد کالا";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "تعداد";
+            // 
+            // tbxGoodCode
+            // 
+            this.tbxGoodCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxGoodCode.Location = new System.Drawing.Point(106, 6);
+            this.tbxGoodCode.Name = "tbxGoodCode";
+            this.tbxGoodCode.Size = new System.Drawing.Size(121, 21);
+            this.tbxGoodCode.TabIndex = 3;
+            this.tbxGoodCode.Text = "11001001";
+            // 
+            // tbxGoodQty
+            // 
+            this.tbxGoodQty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxGoodQty.Location = new System.Drawing.Point(106, 33);
+            this.tbxGoodQty.Name = "tbxGoodQty";
+            this.tbxGoodQty.Size = new System.Drawing.Size(121, 21);
+            this.tbxGoodQty.TabIndex = 4;
+            this.tbxGoodQty.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(280, 98);
+            this.Controls.Add(this.tbxGoodQty);
+            this.Controls.Add(this.tbxGoodCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("IRANSansMobile", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ایجاد رسید انبار";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxGoodCode;
+        private System.Windows.Forms.TextBox tbxGoodQty;
+    }
+}
+
---
+++ BehTestAPI/Form1.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateInvDocByAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        protected async Task Submit()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://tous.behandish.com/");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW4iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiIiLCJqdGkiOiIyYmNhMzgyNy1jZTZiLTRkMmYtYTUzYy1hMWI2YzNiNTE2M2UiLCJleHAiOjE3MDM5MTQ1NTgsImlzcyI6IldlYkJlaGFuZGlzaCIsImF1ZCI6IldlYkJlaGFuZGlzaCJ9.2DMsXHrq9s0ZJ8clvNunrinmbXgV2WZV_kbwxPd6Rk4");
+                client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var json = JsonConvert.SerializeObject(new 
+                {
+                    #region init
+                    ID = 0,
+                    DocStatus = 1,
+                    
+                    #endregion
+
+                    InvDocType = 1, //نوع  رویداد: رسید انبار
+                    DocDate = "1402/07/18", // تاریخ
+                    InvDocKind = new { ID = 20193106 }, // نوع
+                    Comments = "ایجاد توسط اي پي آي",
+                    QtyComments = "ایجاد توسط اي پي آي",
+                    QtyStatus = 2048, //وضعیت: تایید مقداری // 1024
+                    Stock = new { ID = 5611867 }, // انبار سنگان معدن
+                    items = new List<object>() 
+                    {
+                        new 
+                        {
+                            ID = 0,
+                            RowNo = 1,
+                            Dsc = "ایجاد سطر توسط اي پي آي",
+                            Good = new { Code = Convert.ToInt32(tbxGoodCode.Text) },
+                            InputQty = Convert.ToInt32(tbxGoodQty.Text),
+                            UnitID = (string) null,
+                            SubSb = (string) null,
+                        }
+                    },
+                });
+                var invDoc = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("API/Inventory/InvDocs?InvDocType=1&IsPricedDoc=false", invDoc);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var strResponse = await response.Content.ReadAsStringAsync();
+                    var jsonResponse = JObject.Parse(strResponse);
+                    MessageBox.Show($"رسيد انبار جديد با شماره: {jsonResponse.GetValue("DocNo")} و شناسه: {jsonResponse.GetValue("ID")} در انبار: سنگان معدن ايجاد شد", 
+                                     "پیام", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"{response.StatusCode}:\n{message}", "پیام", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        protected void Reset()
+        {
+            tbxGoodCode.Text = "11001001";
+            tbxGoodQty.Text = "1";
+        }
+
+        private async void btnSubmit_Click(object sender, EventArgs e)
+        {
+            await Submit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
